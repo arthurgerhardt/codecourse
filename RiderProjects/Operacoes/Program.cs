dc01844ce20927c5965661cdcbbf6359f9e688dc
Arthur Dias Gomes Gerhardt <66851119+arthurgerhardt@users.noreply.github.com>
2024-11-27 21:28:15 -0300

    Add files via upload

---
+++ RiderProjects/Operacoes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Operacoes
+{
+    // Classe Program com o método Main
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numero = 0;
+            do
+            {
+                Console.WriteLine("Teste");
+                numero++;
+            } while (numero < 10);
+            Console.WriteLine("Saiu do while.");
+        }
+    }
+}
