390706a13e5252f4eff99bb20aca332be6d44fb7
Arthur Gerhardt <renewedjesus@gmail.com>
2025-01-13 12:18:49 -0300

    Initial Commit

---
+++ vite-project/src/Post.jsx
@@ -0,0 +1,11 @@
+// props: { author: "John Doe", title: "Post Title" }
+export function Post() {
+  return (
+    <div>
+      <h1>Post</h1>
+    </div>
+  )
+}
+//export default Post
+
+// Default Export vs Named Export
